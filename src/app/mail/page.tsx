5ccf5eca0119072cf2802d275ca54f818ff867c2
anvv <anvv@smartosc.com>
2024-03-20 09:55:13 +0700

    new

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="h-[52px]"></div>
       <Separator />
       <div className="flex grow justify-center items-center">
-        <h2 className="text-3xl font-semibold">Choose an inbox to begin</h2>
+        <h2 className="text-3xl font-semibold">Choose an inbox to begin - new</h2>
         <p className="text-3xl font-semibold">Main branch</p>
       </div>
     </section>
